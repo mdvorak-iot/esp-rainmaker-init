93dcd7b45cb67161a5f4faa2ec564610be129e1a
Michal Dvorak <michal@mdvorak.org>
2021-04-16 20:43:31 +0200

    fixed example

--- example/main/rainmaker_init_example_main.c
+++ example/main/rainmaker_init_example_main.c
@@ -1,12 +1,14 @@
-#include "app_rainmaker.h"
+#include <app_rainmaker.h>
 #include <app_wifi.h>
 #include <esp_log.h>
 #include <esp_rmaker_core.h>
 #include <esp_rmaker_standard_params.h>
-#include <esp_rmaker_standard_types.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
 #include <string.h>
+
+#define APP_DEVICE_NAME CONFIG_APP_DEVICE_NAME
+#define APP_DEVICE_TYPE CONFIG_APP_DEVICE_TYPE
 
 static const char TAG[] = "app_main";
 
@@ -28,24 +30,24 @@ void setup()
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     // Setup
-    char node_name[APP_RMAKER_NODE_NAME_LEN] = {};
-    ESP_ERROR_CHECK(app_rmaker_node_name(node_name, sizeof(node_name)));
-
     struct app_wifi_config wifi_cfg = {
         .security = WIFI_PROV_SECURITY_1,
-        .hostname = node_name,
         .wifi_connect = esp_wifi_connect,
     };
     ESP_ERROR_CHECK(app_wifi_init(&wifi_cfg));
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_MAX_MODEM));
 
     // RainMaker
+    char node_name[APP_RMAKER_NODE_NAME_LEN] = {};
+    ESP_ERROR_CHECK(app_rmaker_node_name(node_name, sizeof(node_name)));
+
     esp_rmaker_node_t *node = NULL;
     ESP_ERROR_CHECK(app_rmaker_init(node_name, &node));
 
     app_devices_init(node);
 
     // Start
+    ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, node_name)); // NOTE this isn't available before WiFi init
     ESP_ERROR_CHECK(esp_rmaker_start());
     ESP_ERROR_CHECK(app_wifi_start(false));
 
@@ -71,13 +73,11 @@ static esp_err_t device_write_cb(__unused const esp_rmaker_device_t *device, con
 static void app_devices_init(esp_rmaker_node_t *node)
 {
     // Prepare device
-    esp_rmaker_device_t *device = esp_rmaker_device_create("Example", ESP_RMAKER_DEVICE_SWITCH, NULL);
+    esp_rmaker_device_t *device = esp_rmaker_device_create(APP_DEVICE_NAME, APP_DEVICE_TYPE, NULL);
     assert(device);
 
     ESP_ERROR_CHECK(esp_rmaker_device_add_cb(device, device_write_cb, NULL));
-
-    char *node_name = esp_rmaker_node_get_info(node)->name;
-    ESP_ERROR_CHECK(esp_rmaker_device_add_param(device, esp_rmaker_name_param_create(ESP_RMAKER_DEF_NAME_PARAM, node_name)));
+    ESP_ERROR_CHECK(esp_rmaker_device_add_param(device, esp_rmaker_name_param_create(ESP_RMAKER_DEF_NAME_PARAM, APP_DEVICE_NAME)));
     ESP_ERROR_CHECK(esp_rmaker_node_add_device(node, device));
 
     // Register buttons, sensors, etc
